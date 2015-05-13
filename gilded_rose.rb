cea397fffdb38a1cdd0ee9c0e1b5e1068e03664f
Erwan Guyader <erwan@coverhound.com>
2015-05-12 21:59:38 -0700

    My Solution

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,49 +1,74 @@
+Sulfuras = "Sulfuras, Hand of Ragnaros"
+Backstage = "Backstage passes to a TAFKAL80ETC concert"
+Aged_Brie = "Aged Brie"
+Conjured = "Conjured Mana Cake"
+
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-    end
+    modify_quality(item)
+    decrease_sellin(item)
   end
+end
+
+def modify_quality(item)
+  amount = modifier(item)
+
+  return if amount.nil?
+
+  new_quality = (amount == 0) ? 0 : item.quality + amount
+  new_quality = 50 if new_quality > 50
+  new_quality = 0 if new_quality < 0
+
+  item.quality = new_quality
+end
+
+def decrease_sellin(item)
+  return if item.name == Sulfuras
+
+  item.sell_in -= 1
+end
+
+def modifier(item)
+  case item.name
+  when Sulfuras
+    sulfuras_modifier(item)
+  when Backstage
+    backstage_modifier(item)
+  when Aged_Brie
+    aged_brie_modifier(item)
+  when Conjured
+    conjured_modifier(item)
+  else
+    default_modifier(item)
+  end
+end
+
+def sulfuras_modifier(item)
+  nil
+end
+
+def aged_brie_modifier(item)
+  item.sell_in > 0 ? 1 : 2
+end
+
+def backstage_modifier(item)
+  if item.sell_in <= 0
+    0
+  elsif item.sell_in < 6
+    3
+  elsif item.sell_in < 11
+    2
+  else
+    1
+  end
+end
+
+def conjured_modifier(item)
+  default_modifier(item) * 2
+end
+
+def default_modifier(item)
+  item.sell_in > 0 ? -1 : -2
 end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
